3677e07c8b16e9d27574dd0b25f9008479dabe36
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-01-12 21:01:05 +0100

    + fill-fast.ps1

---
+++ Excel/Range/value/fill-fast.ps1
@@ -0,0 +1,39 @@
+# vi: ft=conf
+$xls = new-object -com excel.application
+$xls.visible = $true
+
+$wb  = $xls.workbooks.add()
+$sh  = $wb.Worksheets.add()
+
+
+(measure-command {
+#
+#  Uncommenting the following three statements
+#  seem to slow down (rather than speed up)
+#  the execution!
+#
+#  $xls.calculation    = -4135
+#  $xls.screenUpdating =  $false
+#  $xls.enableEvents   =  $false
+
+   $nofRows = 100
+   $nofCols =  50
+
+   $values = new-object 'Object[,]' $nofRows, $nofCols
+
+   foreach ($r in 0 .. ($nofRows-1) ) {
+   foreach ($c in 0 .. ($nofCols-1) ) {
+      $values[$r,$c] = "${r} * $c = $($r*$c)"
+   }}
+
+   $sh.range(
+      $sh.cells(       1,       1),
+      $sh.cells($nofRows,$nofCols)
+   ).value = $values
+
+#  $xls.screenUpdating =  $true
+#  $xls.enableEvents   =  $true
+#  $xls.calculation    = -4105
+}).TotalSeconds
+
+$xls = $null
