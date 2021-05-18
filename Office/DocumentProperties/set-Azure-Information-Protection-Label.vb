b4e4c9083646aca1e7078c102220591eea5fbd84
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-18 15:52:42 +0200

    remove debug.print

--- Office/DocumentProperties/set-Azure-Information-Protection-Label.vb
+++ Office/DocumentProperties/set-Azure-Information-Protection-Label.vb
@@ -8,8 +8,6 @@ sub mai() ' {
     dim guid_internal_use_only as string
         guid_internal_use_only = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
-    debug.print typename(wb.customDocumentProperties)
-
     wb.customDocumentProperties.add "MSIP_Label_" & guid_internal_use_only & "_Enabled", false, msoPropertyTypeString, "true"
 
     wb.saveAs environ("temp") & "\created-at-" & format(now(), "yyyy-mm-dd_hh-nn")
